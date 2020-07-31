a2225a3149301e93d68cc742f421d52642091b5a
Eddy Wilson <eddyw@users.noreply.github.com>
2020-07-31 14:25:36 +0800

    chore: add rollup config

---
+++ config/rollup.config.js
@@ -0,0 +1,41 @@
+import * as path from 'path'
+import typescript from '@wessberg/rollup-plugin-ts'
+import filesize from 'rollup-plugin-filesize'
+
+// https://github.com/rollup/rollup/issues/1089
+const onwarn = (warning, rollupWarn) => {
+	const ignoredWarnings = [
+		{
+			ignoredCode: 'CIRCULAR_DEPENDENCY',
+			ignoredPath: 'src/tokenizer/algorithms/index.ts',
+		},
+	]
+	if (
+		!ignoredWarnings.some(
+			({ ignoredCode, ignoredPath }) =>
+				warning.code === ignoredCode && warning.importer.includes(path.normalize(ignoredPath)),
+		)
+	) {
+		rollupWarn(warning)
+	}
+}
+
+export default {
+	input: 'src/index.ts',
+	onwarn,
+	output: [
+		{
+			file: 'dist/index.cjs',
+			format: 'cjs',
+			strict: true,
+			sourcemap: true,
+		},
+		{
+			file: 'dist/index.mjs',
+			format: 'esm',
+			strict: true,
+			sourcemap: true,
+		},
+	],
+	plugins: [typescript(), filesize({ showBrotliSize: true, theme: 'light' })],
+}
