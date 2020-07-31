cd3706e2e81e53cc21996c0deb2f60fdfbe03d86
Eddy Wilson <eddyw@users.noreply.github.com>
2020-07-31 14:27:05 +0800

    fix(jest): point root to src

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils')
 
 module.exports = {
-	roots: ['<rootDir>/lib'],
+	roots: ['<rootDir>/src'],
 	testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
 	transform: {
 		'^.+\\.(ts|tsx)$': 'ts-jest',
 	},
 	moduleNameMapper: pathsToModuleNameMapper({
-		'~/*': ['lib/*'],
+		'~/*': ['src/*'],
 	}),
 	globals: {
 		'ts-jest': {
