97830351f3783ab258bb3a777bdf4c48dbf5f01e
Eddy Wilson <eddyw@users.noreply.github.com>
2020-08-03 20:57:32 +0800

    feat: use plain function instead of generator

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { tokenizer } from './tokenizer'
 export { createContext } from './shared/context'
-export type { TokenizerYield } from './tokenizer'
+export type { TokenizerReturnToken } from './tokenizer'
 export type { TokenizerContext } from './shared/context'
