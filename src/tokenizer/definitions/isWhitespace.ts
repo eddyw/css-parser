7904649245d252a2540c5dda476c239e00d1c6dd
Eddy Wilson <eddyw@users.noreply.github.com>
2020-08-05 00:37:06 +0800

    fix(definitions): rename and comment

--- src/tokenizer/definitions/isWhitespace.ts
+++ src/tokenizer/definitions/isWhitespace.ts
@@ -7,8 +7,7 @@ import { isNewline } from '.'
  * A newline, U+0009 CHARACTER TABULATION, or U+0020 SPACE.
  */
 export function isWhitespace(code: number): boolean {
-	return isNewline(code) || code === WHITESPACE.SPC || code === WHITESPACE.TAB
-}
-export function isTabOrSpace(code: number): boolean {
-	return code === WHITESPACE.SPC || code === WHITESPACE.TAB
+	return (
+		isNewline(code) || code === WHITESPACE.SPC || code === WHITESPACE.TAB
+	)
 }
