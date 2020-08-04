f34963210fb478199148a0e49c35b165bd9bd000
Eddy Wilson <eddyw@users.noreply.github.com>
2020-08-05 00:35:36 +0800

    fix(context): remove token line/column

--- src/shared/context.ts
+++ src/shared/context.ts
@@ -13,13 +13,7 @@ export interface TokenizerContext {
 	tokenLead: number
 	tokenTail: number
 	tokenFlag: number
-	tokenLineOpen: number
-	tokenLineShut: number
-	tokenColumnOpen: number
-	tokenColumnShut: number
 	setCodePointAtCurrent(): void
-	setLineAtCurrent(): void
-	consumeAtCurrent(n: number): void
 }
 
 export function createContext(css: string): TokenizerContext {
@@ -36,10 +30,6 @@ export function createContext(css: string): TokenizerContext {
 		tokenLead: 0,
 		tokenTail: 0,
 		tokenFlag: 0,
-		tokenColumnOpen: 1,
-		tokenColumnShut: 1,
-		tokenLineOpen: 1,
-		tokenLineShut: 1,
 		setCodePointAtCurrent() {
 			const posAt0 = this.tokenShut
 			const posAt1 = posAt0 + 1
@@ -50,14 +40,6 @@ export function createContext(css: string): TokenizerContext {
 			this.charAt1 = posAt1 >= this.sourceSize ? TOKEN.EOF : this.source.charCodeAt(posAt1)
 			this.charAt2 = posAt2 >= this.sourceSize ? TOKEN.EOF : this.source.charCodeAt(posAt2)
 			this.charAt3 = posAt3 >= this.sourceSize ? TOKEN.EOF : this.source.charCodeAt(posAt3)
-		},
-		setLineAtCurrent() {
-			this.tokenLineShut += 1 // Consume « white-space »
-			this.tokenColumnShut = 1
-		},
-		consumeAtCurrent(nChars: number) {
-			this.tokenShut += nChars
-			this.tokenColumnShut += nChars
 		},
 	}
 
