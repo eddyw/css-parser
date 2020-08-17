ba67b5d9447e58c90afac61276b42b8a0d960720
Eddy Wilson <eddyw@users.noreply.github.com>
2020-08-18 01:16:22 +0800

    feat(consume-comment): return node

--- src/tokenizer/algorithms/consumeComments.ts
+++ src/tokenizer/algorithms/consumeComments.ts
@@ -1,5 +1,5 @@
-import { TOKEN, TYPE, FLAGS_ALL } from '~/constants'
-import type { TokenizerContext } from '~/shared/types'
+import { TOKEN, NODE_SYMB, NODE_TYPE, FLAG_ANY } from '~/constants'
+import type { TokenizerContext, CSSComment } from '~/shared/types'
 
 /**
  * @see https://drafts.csswg.org/css-syntax/#consume-comment
@@ -16,24 +16,37 @@ import type { TokenizerContext } from '~/shared/types'
  * Return nothing.
  *
  * @note - ours
- * It assumes the next two input code points « U+002F SOLIDUS (/) followed by a U+002A ASTERISK (*) »
+ * - It assumes the next two input code points « U+002F SOLIDUS (/) followed by a U+002A ASTERISK (*) »
  * were already verified to be comment start
+ * - It returns something instead of nothing (CSSComment token)
  */
-export function consumeComments(ctx: TokenizerContext): void {
-	ctx.tokenType = TYPE.COMMENT
-	ctx.tokenLead = 2
-	ctx.tokenShut += 2 // Consume «U+002F SOLIDUS (/) followed by a U+002A ASTERISK (*)»
+export function consumeComments(x: TokenizerContext): Readonly<CSSComment> {
+	x.lead = 2
+	x.shut += 2 // Consume «U+002F SOLIDUS (/) followed by a U+002A ASTERISK (*)»
 
-	for (; ctx.tokenShut <= ctx.sourceSize; ctx.tokenShut++) {
-		ctx.setCodePointAtCurrent()
-		if (ctx.charAt0 === TOKEN.ASTERISK && ctx.charAt1 === TOKEN.FORWARD_SOLIDUS) {
-			ctx.tokenShut += 2 // Consume «U+002A ASTERISK (*) followed by a U+002F SOLIDUS (/)»
-			ctx.tokenTail = 2
+	for (; x.shut <= x.size; x.shut++) {
+		x.setCodeAtCurrent()
+		if (x.codeAt0 === TOKEN.ASTERISK && x.codeAt1 === TOKEN.FORWARD_SOLIDUS) {
+			x.shut += 2 // Consume «U+002A ASTERISK (*) followed by a U+002F SOLIDUS (/)»
+			x.tail = 2
 			break
-		} else if (ctx.charAt0 === TOKEN.EOF) {
-			ctx.tokenFlag |= FLAGS_ALL.IS_PARSE_ERROR
-			ctx.tokenShut = ctx.sourceSize
+		} else if (x.codeAt0 === TOKEN.EOF) {
+			x.flag |= FLAG_ANY.PARSE_ERROR
+			x.shut = x.size
 			break
 		}
+	}
+
+	return {
+		type: NODE_TYPE.COMMENT_TOKEN,
+		symb: NODE_SYMB.COMMENT_TOKEN,
+		flag: x.flag,
+		node: x.code.slice(x.open + x.lead, x.shut - x.tail),
+		open: x.code.slice(x.open, x.open + x.lead) as '/*',
+		shut: x.code.slice(x.shut - x.tail, x.shut) as '*/',
+		spot: {
+			offsetIni: x.open,
+			offsetEnd: x.shut,
+		},
 	}
 }
