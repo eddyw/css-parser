52641c8c6023dcb0e57f4d7702d4d27bbdf274e7
Eddy Wilson <eddyw@users.noreply.github.com>
2020-08-18 01:10:53 +0800

    feat(context): use codePointAt
    - fix types

--- src/shared/context.ts
+++ src/shared/context.ts
@@ -1,30 +1,30 @@
-import { TOKEN, TYPE } from '~/constants'
+import { TOKEN, NODE_SYMB } from '~/constants'
 import type { TokenizerContext } from './types'
 
 export function createContext(css: string): TokenizerContext {
 	const context: TokenizerContext = {
-		source: css,
-		sourceSize: css.length,
-		charAt0: TOKEN.EOF,
-		charAt1: TOKEN.EOF,
-		charAt2: TOKEN.EOF,
-		charAt3: TOKEN.EOF,
-		tokenType: TYPE.EOF,
-		tokenOpen: 0,
-		tokenShut: 0,
-		tokenLead: 0,
-		tokenTail: 0,
-		tokenFlag: 0,
-		setCodePointAtCurrent() {
-			const posAt0 = this.tokenShut
+		code: css,
+		size: css.length,
+		codeAt0: TOKEN.EOF,
+		codeAt1: TOKEN.EOF,
+		codeAt2: TOKEN.EOF,
+		codeAt3: TOKEN.EOF,
+		type: NODE_SYMB.END_OF_FILE,
+		open: 0,
+		shut: 0,
+		lead: 0,
+		tail: 0,
+		flag: 0,
+		setCodeAtCurrent() {
+			const posAt0 = this.shut
 			const posAt1 = posAt0 + 1
 			const posAt2 = posAt0 + 2
 			const posAt3 = posAt0 + 3
 
-			this.charAt0 = posAt0 >= this.sourceSize ? TOKEN.EOF : this.source.charCodeAt(posAt0)
-			this.charAt1 = posAt1 >= this.sourceSize ? TOKEN.EOF : this.source.charCodeAt(posAt1)
-			this.charAt2 = posAt2 >= this.sourceSize ? TOKEN.EOF : this.source.charCodeAt(posAt2)
-			this.charAt3 = posAt3 >= this.sourceSize ? TOKEN.EOF : this.source.charCodeAt(posAt3)
+			this.codeAt0 = posAt0 >= this.size ? TOKEN.EOF : this.code.codePointAt(posAt0)!
+			this.codeAt1 = posAt1 >= this.size ? TOKEN.EOF : this.code.codePointAt(posAt1)!
+			this.codeAt2 = posAt2 >= this.size ? TOKEN.EOF : this.code.codePointAt(posAt2)!
+			this.codeAt3 = posAt3 >= this.size ? TOKEN.EOF : this.code.codePointAt(posAt3)!
 		},
 	}
 
