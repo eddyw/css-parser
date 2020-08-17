d6ea4fea05c4415344c125e1bd2060fa1edce105
Eddy Wilson <eddyw@users.noreply.github.com>
2020-08-18 01:09:16 +0800

    feat(TokenFlag): specify flags per node type

--- src/constants/TokenFlag.ts
+++ src/constants/TokenFlag.ts
@@ -1,12 +1,27 @@
-export const enum FLAGS_ALL {
-	IS_PARSE_ERROR = 1 << 0,
+export const enum FLAG_ANY {
+	PARSE_ERROR = 1 << 0,
 }
-export const enum FLAGS_NUMBER {
-	IS_PARSE_ERROR = 1 << 0,
-	IS_INTEGER = 1 << 1,
-	IS_DOUBLE = 1 << 2,
+export const enum FLAG_NUMBER {
+	PARSE_ERROR = 1 << 0,
+	INTEGER = 1 << 1,
+	DOUBLE = 1 << 2,
 }
-export const enum FLAGS_HASH {
-	IS_PARSE_ERROR = 1 << 0,
-	IS_ID = 1 << 1,
+export const enum FLAG_STRING {
+	PARSE_ERROR = 1 << 0,
+	BAD_STRING = 1 << 1,
+	END_IS_EOF = 1 << 2,
+	END_IS_ESCAPED_EOF = 1 << 3,
+	END_IS_NEWLINE = 1 << 4,
+}
+export const enum FLAG_IDENTIFIER {
+	PARSE_ERROR = 1 << 0,
+	FUNCTION_NAME = 1 << 1,
+	HASH_IDENT = 1 << 2,
+}
+export const enum FLAG_URL {
+	PARSE_ERROR = 1 << 0,
+	BAD_URL = 1 << 1,
+	END_IS_EOF = 1 << 2,
+	NON_PRINTABLE = 1 << 3,
+	BAD_ESCAPE = 1 << 4,
 }
