a748ec82bb57fa53dc73b4405062a124ee58293f
Eddy Wilson <eddyw@users.noreply.github.com>
2020-08-18 01:22:18 +0800

    feat(tokenizer): use methods to return nodes

--- src/tokenizer/tokenizer.ts
+++ src/tokenizer/tokenizer.ts
@@ -1,161 +1,174 @@
-import { TOKEN, TYPE, FLAGS_ALL, FLAGS_HASH } from '~/constants'
+import { TOKEN, NODE_SYMB, FLAG_ANY } from '~/constants'
 import {
 	areIdentifierNameStart,
 	areNumberStart,
 	areValidEscape,
 	isDigit,
 	isIdentifierStartCodePoint,
-	isIdentifierCodePoint,
 	isWhitespace,
 } from './definitions'
 import {
 	consumeComments,
 	consumeIdentLikeToken,
-	consumeIdentifier,
 	consumeNumericToken,
 	consumeStringToken,
 	consumeWhitespace,
+	consumeHash,
+	consumeOpenParenthesis,
+	consumeShutParenthesis,
+	consumeDelimToken,
+	consumeCommaToken,
+	consumeCDCToken,
+	consumeCDOToken,
+	consumeEndOfFile,
+	consumeColonToken,
+	consumeSemiToken,
+	consumeOpenSquareBracket,
+	consumeShutSquareBracket,
+	consumeOpenCurlyBrace,
+	consumeShutCurlyBrace,
+	consumeAtKeywordToken,
 } from './algorithms'
-import type { TokenizerReturnToken, TokenizerContext } from '~/shared/types'
+import type { TokenizerContext, Tokenizer, CSSToken } from '~/shared/types'
 
-export function tokenizer(ctx: TokenizerContext) {
-	function consumeToken(): TokenizerReturnToken {
-		if (ctx.tokenShut > ctx.sourceSize) throw Error('Out of bounds')
-		ctx.setCodePointAtCurrent()
-		ctx.tokenType = TYPE.EOF
-		ctx.tokenOpen = ctx.tokenShut
-		ctx.tokenLead = 0
-		ctx.tokenTail = 0
-		ctx.tokenFlag = 0
+const up = Error('Out of bounds')
 
-		if (ctx.tokenShut === ctx.sourceSize) {
-			ctx.tokenType = TYPE.EOF
-			ctx.tokenShut += 1
-		} else if (ctx.charAt0 === TOKEN.FORWARD_SOLIDUS && ctx.charAt1 === TOKEN.ASTERISK) {
-			consumeComments(ctx)
-		} else if (isWhitespace(ctx.charAt0)) {
-			ctx.tokenType = TYPE.WHITESPACE
-			ctx.tokenShut += 1
-			consumeWhitespace(ctx)
-		} else if (ctx.charAt0 === TOKEN.DOUBLE_QUOTE) {
-			consumeStringToken(ctx, ctx.charAt0)
-		} else if (ctx.charAt0 === TOKEN.HASH) {
-			if (isIdentifierCodePoint(ctx.charAt1) || areValidEscape(ctx.charAt1, ctx.charAt2)) {
-				ctx.tokenType = TYPE.HASH
-				ctx.tokenLead = 1
+/**
+ * @see https://drafts.csswg.org/css-syntax/#consume-token
+ * @description § 4.3.1. Consume a token
+ */
+export function tokenizer(x: TokenizerContext): Tokenizer {
+	function consumeToken(): CSSToken {
+		if (x.shut > x.size) throw up
 
-				if (areIdentifierNameStart(ctx.charAt1, ctx.charAt2, ctx.charAt3)) {
-					ctx.tokenFlag |= FLAGS_HASH.IS_ID
-				}
-				ctx.tokenShut += 1
-				consumeIdentifier(ctx)
-			} else {
-				ctx.tokenShut += 1
-				ctx.tokenType = TYPE.DELIMITER
-			}
-		} else if (ctx.charAt0 === TOKEN.SINGLE_QUOTE) {
-			consumeStringToken(ctx, ctx.charAt0)
-		} else if (ctx.charAt0 === TOKEN.L_PARENTHESIS) {
-			ctx.tokenShut += 1
-			ctx.tokenType = TYPE.L_PARENTHESIS
-		} else if (ctx.charAt0 === TOKEN.R_PARENTHESIS) {
-			ctx.tokenShut += 1
-			ctx.tokenType = TYPE.R_PARENTHESIS
-		} else if (ctx.charAt0 === TOKEN.PLUS) {
-			if (areNumberStart(ctx.charAt0, ctx.charAt1, ctx.charAt2)) {
-				consumeNumericToken(ctx)
-			} else {
-				ctx.tokenShut += 1
-				ctx.tokenType = TYPE.DELIMITER
-			}
-		} else if (ctx.charAt0 === TOKEN.COMMA) {
-			ctx.tokenShut += 1
-			ctx.tokenType = TYPE.COMMA
-		} else if (ctx.charAt0 === TOKEN.MINUS) {
-			if (areNumberStart(ctx.charAt0, ctx.charAt1, ctx.charAt2)) {
-				consumeNumericToken(ctx)
-			} else {
-				if (ctx.charAt1 === TOKEN.MINUS && ctx.charAt2 === TOKEN.GREATER_THAN) {
-					ctx.tokenShut += 3 // Consume (-->)
-					ctx.tokenType = TYPE.CDC
-				} else if (areIdentifierNameStart(ctx.charAt0, ctx.charAt1, ctx.charAt2)) {
-					consumeIdentLikeToken(ctx)
-				} else {
-					ctx.tokenType = TYPE.DELIMITER
-					ctx.tokenShut += 1
-				}
-			}
-		} else if (ctx.charAt0 === TOKEN.STOP) {
-			if (areNumberStart(ctx.charAt0, ctx.charAt1, ctx.charAt2)) {
-				consumeNumericToken(ctx)
-			} else {
-				ctx.tokenShut += 1
-				ctx.tokenType = TYPE.DELIMITER
-			}
-		} else if (ctx.charAt0 === TOKEN.COLON) {
-			ctx.tokenType = TYPE.COLON
-			ctx.tokenShut += 1
-		} else if (ctx.charAt0 === TOKEN.SEMI) {
-			ctx.tokenType = TYPE.SEMI
-			ctx.tokenShut += 1
-		} else if (ctx.charAt0 === TOKEN.LESS_THAN) {
-			if (ctx.charAt1 === TOKEN.EXCLAMATION && ctx.charAt2 === TOKEN.MINUS && ctx.charAt3 === TOKEN.MINUS) {
-				ctx.tokenType = TYPE.CDO
-				ctx.tokenShut += 4 // Consume (<!--)
-			} else {
-				ctx.tokenType = TYPE.DELIMITER
-				ctx.tokenShut += 1
-			}
-		} else if (ctx.charAt0 === TOKEN.AT) {
-			ctx.tokenShut += 1
-			if (areIdentifierNameStart(ctx.charAt1, ctx.charAt2, ctx.charAt3)) {
-				ctx.tokenType = TYPE.AT_RULE
-				consumeIdentifier(ctx)
-			} else {
-				ctx.tokenType = TYPE.DELIMITER
-			}
-		} else if (ctx.charAt0 === TOKEN.L_SQUARE_BRACKET) {
-			ctx.tokenType = TYPE.L_SQUARE_BRACKET
-			ctx.tokenShut += 1
-		} else if (ctx.charAt0 === TOKEN.REVERSE_SOLIDUS) {
-			if (areValidEscape(ctx.charAt0, ctx.charAt1)) {
-				consumeIdentLikeToken(ctx)
-			} else {
-				ctx.tokenFlag = FLAGS_ALL.IS_PARSE_ERROR
-				ctx.tokenType = TYPE.DELIMITER
-				ctx.tokenShut += 1
-			}
-		} else if (ctx.charAt0 === TOKEN.R_SQUARE_BRACKET) {
-			ctx.tokenType = TYPE.R_SQUARE_BRACKET
-			ctx.tokenShut += 1
-		} else if (ctx.charAt0 === TOKEN.L_CURLY_BRACKET) {
-			ctx.tokenType = TYPE.L_CURLY_BRACKET
-			ctx.tokenShut += 1
-		} else if (ctx.charAt0 === TOKEN.R_CURLY_BRACKET) {
-			ctx.tokenType = TYPE.R_CURLY_BRACKET
-			ctx.tokenShut += 1
-		} else if (isDigit(ctx.charAt0)) {
-			consumeNumericToken(ctx)
-		} else if (isIdentifierStartCodePoint(ctx.charAt0)) {
-			consumeIdentLikeToken(ctx)
-		} else {
-			ctx.tokenType = TYPE.DELIMITER
-			ctx.tokenShut += 1
+		x.setCodeAtCurrent()
+		x.type = NODE_SYMB.END_OF_FILE as NODE_SYMB
+		x.open = x.shut
+		x.lead = 0
+		x.tail = 0
+		x.flag = 0
+
+		if (x.codeAt0 === TOKEN.FORWARD_SOLIDUS && x.codeAt1 === TOKEN.ASTERISK) {
+			return consumeComments(x)
 		}
 
-		return {
-			tokenType: ctx.tokenType,
-			tokenOpen: ctx.tokenOpen,
-			tokenShut: ctx.tokenShut,
-			tokenTail: ctx.tokenTail,
-			tokenLead: ctx.tokenLead,
-			tokenFlag: ctx.tokenFlag,
+		if (isWhitespace(x.codeAt0)) {
+			return consumeWhitespace(x)
 		}
+
+		if (x.codeAt0 === TOKEN.DOUBLE_QUOTE || x.codeAt0 === TOKEN.SINGLE_QUOTE) {
+			return consumeStringToken(x, x.codeAt0)
+		}
+
+		if (x.codeAt0 === TOKEN.HASH) {
+			return consumeHash(x)
+		}
+
+		if (x.codeAt0 === TOKEN.L_PARENTHESIS) {
+			return consumeOpenParenthesis(x)
+		}
+
+		if (x.codeAt0 === TOKEN.R_PARENTHESIS) {
+			return consumeShutParenthesis(x)
+		}
+
+		if (x.codeAt0 === TOKEN.PLUS) {
+			if (areNumberStart(x.codeAt0, x.codeAt1, x.codeAt2)) {
+				return consumeNumericToken(x)
+			}
+			return consumeDelimToken(x, TOKEN.PLUS, '+')
+		}
+
+		if (x.codeAt0 === TOKEN.COMMA) {
+			return consumeCommaToken(x)
+		}
+
+		if (x.codeAt0 === TOKEN.MINUS) {
+			if (areNumberStart(x.codeAt0, x.codeAt1, x.codeAt2)) {
+				return consumeNumericToken(x)
+			}
+
+			if (x.codeAt1 === TOKEN.MINUS && x.codeAt2 === TOKEN.GREATER_THAN) {
+				return consumeCDCToken(x)
+			} else if (areIdentifierNameStart(x.codeAt0, x.codeAt1, x.codeAt2)) {
+				return consumeIdentLikeToken(x)
+			} else {
+				return consumeDelimToken(x, TOKEN.MINUS, '-')
+			}
+		}
+
+		if (x.codeAt0 === TOKEN.STOP) {
+			if (areNumberStart(x.codeAt0, x.codeAt1, x.codeAt2)) {
+				return consumeNumericToken(x)
+			}
+			return consumeDelimToken(x, TOKEN.STOP, '.')
+		}
+
+		if (x.codeAt0 === TOKEN.COLON) {
+			return consumeColonToken(x)
+		}
+
+		if (x.codeAt0 === TOKEN.SEMI) {
+			return consumeSemiToken(x)
+		}
+
+		if (x.codeAt0 === TOKEN.LESS_THAN) {
+			if (x.codeAt1 === TOKEN.EXCLAMATION && x.codeAt2 === TOKEN.MINUS && x.codeAt3 === TOKEN.MINUS) {
+				return consumeCDOToken(x)
+			}
+			return consumeDelimToken(x, TOKEN.LESS_THAN, '<')
+		}
+
+		if (x.codeAt0 === TOKEN.AT) {
+			if (areIdentifierNameStart(x.codeAt1, x.codeAt2, x.codeAt3)) {
+				return consumeAtKeywordToken(x)
+			}
+			return consumeDelimToken(x, TOKEN.AT, '@')
+		}
+
+		if (x.codeAt0 === TOKEN.L_SQUARE_BRACKET) {
+			return consumeOpenSquareBracket(x)
+		}
+
+		if (x.codeAt0 === TOKEN.REVERSE_SOLIDUS) {
+			if (areValidEscape(x.codeAt0, x.codeAt1) && x.codeAt1 !== TOKEN.EOF) {
+				return consumeIdentLikeToken(x)
+			}
+			x.flag |= FLAG_ANY.PARSE_ERROR
+			return consumeDelimToken(x, TOKEN.REVERSE_SOLIDUS, '\\')
+		}
+
+		if (x.codeAt0 === TOKEN.R_SQUARE_BRACKET) {
+			return consumeShutSquareBracket(x)
+		}
+
+		if (x.codeAt0 === TOKEN.L_CURLY_BRACKET) {
+			return consumeOpenCurlyBrace(x)
+		}
+
+		if (x.codeAt0 === TOKEN.R_CURLY_BRACKET) {
+			return consumeShutCurlyBrace(x)
+		}
+
+		if (isDigit(x.codeAt0)) {
+			return consumeNumericToken(x)
+		}
+
+		if (isIdentifierStartCodePoint(x.codeAt0)) {
+			return consumeIdentLikeToken(x)
+		}
+
+		if (x.shut === x.size) {
+			return consumeEndOfFile(x)
+		}
+
+		return consumeDelimToken(x, x.codeAt0, String.fromCodePoint(x.codeAt0))
 	}
 
 	return {
+		context: x,
 		isDone() {
-			return ctx.tokenShut > ctx.sourceSize
+			return x.shut > x.size
 		},
 		consumeToken,
 	}
