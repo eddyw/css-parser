2af4efeed112dd9012bc1b45138ca18a4f6cf7c4
Eddy Wilson <eddyw@users.noreply.github.com>
2020-08-18 01:23:53 +0800

    test(getTokens): add check for infinite loop

--- src/tokenizer/__tests__/functions/getTokens.ts
+++ src/tokenizer/__tests__/functions/getTokens.ts
@@ -4,10 +4,19 @@ export function getTokens(css: string) {
 	const stream = tokenizer(createContext(css))
 	const tokens = []
 
+	let i = 0
 	do {
 		const token = stream.consumeToken()
 		tokens.push(token)
-	} while (!stream.isDone())
+		i++
+	} while (!stream.isDone() && i < 200000)
+
+	if (i >= 200000) {
+		console.error('INFINITE LOOP')
+		console.log(`[%o, ..., %o, %o]`, tokens[0], tokens[tokens.length - 2], tokens[tokens.length - 1])
+	} else if (i <= 50) {
+		// console.dir(tokens, { depth: 100 })
+	}
 
 	return tokens
 }
