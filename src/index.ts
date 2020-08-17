724e68232dc5cb8600cfbc51e6709a30a04a4d6e
Eddy Wilson <eddyw@users.noreply.github.com>
2020-08-18 01:23:15 +0800

    chore(tokenizer): export types after methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export type { TokenizerContext, TokenizerReturnToken } from './shared/types'
 export { tokenizer } from './tokenizer'
 export { createContext } from './shared/context'
+export type { TokenizerContext, TokenizerReturnToken } from './shared/types'
