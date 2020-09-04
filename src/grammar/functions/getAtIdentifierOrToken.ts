15ac00e54db242509e6d525a4608e53caf25037b
Eddy Wilson <eddyw@users.noreply.github.com>
2020-09-04 23:54:10 +0800

    feat(grammar): add get at ident

---
+++ src/grammar/functions/getAtIdentifierOrToken.ts
@@ -0,0 +1,40 @@
+import { TOKEN, GRAMMAR_SYMB, GRAMMAR_TYPE } from '~/constants'
+import { getIdentifier, getMultiplierOrToken } from '.'
+import type { GrammarTokenizerContext } from '~/shared/types'
+
+/**
+ * @todo - This sucks
+ * Allow for:
+ * - @func( [block] )
+ * - @ident
+ */
+export function getAtIdentifierOrToken(x: GrammarTokenizerContext) {
+	const open: number = x.shut
+
+	x.consumeCodeAt0(TOKEN.AT)
+
+	const name = getIdentifier(x).node
+
+	if (x.codeAt0 === TOKEN.L_PARENTHESIS) {
+		x.consume(1)
+		return {
+			type: GRAMMAR_TYPE.FUNCTION,
+			symb: GRAMMAR_SYMB.FUNCTION,
+			node: name,
+			spot: {
+				offsetIni: open,
+				offsetEnd: x.shut,
+			},
+		}
+	}
+
+	return getMultiplierOrToken(x, {
+		type: GRAMMAR_TYPE.IDENTIFIER,
+		symb: GRAMMAR_SYMB.IDENTIFIER,
+		node: name,
+		spot: {
+			offsetIni: open,
+			offsetEnd: x.shut,
+		},
+	})
+}
