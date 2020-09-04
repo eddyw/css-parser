b00713a4c75f6135f658b5a5097d7eeb00069e85
Eddy Wilson <eddyw@users.noreply.github.com>
2020-09-04 18:06:32 +0800

    feat(grammar): get number

---
+++ src/grammar/functions/getNumber.ts
@@ -0,0 +1,30 @@
+import { isDigit } from '~/syntax/definitions'
+import { TOKEN } from '~/constants'
+import type { GrammarTokenizerContext } from '~/shared/types'
+
+const up = Error('Expected a number')
+
+export function getNumber(x: GrammarTokenizerContext, signed: boolean) {
+	const open = x.shut
+	let opSign = open
+
+	if (signed && (x.codeAt0 === TOKEN.MINUS || x.codeAt0 === TOKEN.PLUS)) {
+		x.consume(1)
+		opSign += 1
+	}
+
+	while (isDigit(x.codeAt0)) x.consume(1)
+
+	if (opSign === x.shut) throw up
+
+	const node = x.code.slice(open, x.shut)
+
+	return {
+		node: node,
+		repr: Number(node),
+		spot: {
+			offsetIni: open,
+			offsetEnd: x.shut,
+		},
+	}
+}
