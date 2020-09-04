2f3be940282344e2594089d92997acd457ee1594
Eddy Wilson <eddyw@users.noreply.github.com>
2020-09-05 01:30:45 +0800

    fix(grammar): parse entry point as root group

--- src/grammar/parser.ts
+++ src/grammar/parser.ts
@@ -1,7 +1,17 @@
-import { getGroup } from './functions'
+import { getGroupBrackets } from './functions'
 import type { GrammarTokenizerContext } from '~/shared/types'
 
+/**
+ * Parser entry point
+ */
 export function parser(x: GrammarTokenizerContext) {
 	x.setCodeAtCurrent()
-	return getGroup(x, false)
+
+	const group = getGroupBrackets(x, true)
+
+	if (x.shut !== x.size) {
+		throw Error('parser: BUG! PANIC!')
+	}
+
+	return group
 }
