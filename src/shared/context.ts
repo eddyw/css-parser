3c83c1f89e143a4964527e1feba5b069580681d2
Eddy Wilson <eddyw@users.noreply.github.com>
2020-09-04 17:56:30 +0800

    feat: add create grammar context

--- src/shared/context.ts
+++ src/shared/context.ts
@@ -1,7 +1,7 @@
 import { TOKEN, SYNTAX_SYMB } from '~/constants'
-import type { SyntaxTokenizerContext } from './types'
+import type { SyntaxTokenizerContext, GrammarTokenizerContext } from './types'
 
-export function createContext(css: string): SyntaxTokenizerContext {
+export function createSyntaxContext(css: string): SyntaxTokenizerContext {
 	const context: SyntaxTokenizerContext = {
 		code: css,
 		size: css.length,
@@ -26,6 +26,50 @@ export function createContext(css: string): SyntaxTokenizerContext {
 			this.codeAt2 = posAt2 >= this.size ? TOKEN.EOF : this.code.codePointAt(posAt2)!
 			this.codeAt3 = posAt3 >= this.size ? TOKEN.EOF : this.code.codePointAt(posAt3)!
 		},
+	}
+
+	return context
+}
+
+export function createGrammarContext(css: string): GrammarTokenizerContext {
+	const context: GrammarTokenizerContext = {
+		code: css,
+		size: css.length,
+		codeAt0: TOKEN.EOF,
+		codeAt1: TOKEN.EOF,
+		codeAt2: TOKEN.EOF,
+		codeAt3: TOKEN.EOF,
+		type: 0,
+		open: 0,
+		shut: 0,
+		lead: 0,
+		tail: 0,
+		flag: 0,
+		setCodeAtCurrent() {
+			const posAt0 = this.shut
+			const posAt1 = posAt0 + 1
+			const posAt2 = posAt0 + 2
+			const posAt3 = posAt0 + 3
+
+			this.codeAt0 = posAt0 >= this.size ? TOKEN.EOF : this.code.charCodeAt(posAt0)!
+			this.codeAt1 = posAt1 >= this.size ? TOKEN.EOF : this.code.charCodeAt(posAt1)!
+			this.codeAt2 = posAt2 >= this.size ? TOKEN.EOF : this.code.charCodeAt(posAt2)!
+			this.codeAt3 = posAt3 >= this.size ? TOKEN.EOF : this.code.charCodeAt(posAt3)!
+		},
+		consumeCodeAt0(char) {
+			if (this.codeAt0 === char) {
+				this.shut += 1
+				this.setCodeAtCurrent()
+				return
+			}
+			throw Error(
+				`Parse error: Expected token ${String.fromCharCode(char)} but instead got ${String.fromCharCode(this.codeAt0)}`,
+			)
+		},
+		consume(n) {
+			this.shut += n
+			this.setCodeAtCurrent()
+		}
 	}
 
 	return context
