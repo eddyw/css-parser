c5a94862a6ba21e10379aa0be31197230878572e
Eddy Wilson <eddyw@users.noreply.github.com>
2020-09-14 16:43:31 +0800

    fix: use getIdentifierName

--- src/grammar/functions/getAtIdentifierOrToken.ts
+++ src/grammar/functions/getAtIdentifierOrToken.ts
@@ -1,5 +1,5 @@
 import { TOKEN, GRAMMAR_SYMB, GRAMMAR_TYPE } from '~/constants'
-import { getIdentifier, getMultiplierOrToken } from '.'
+import { getIdentifierName, getMultiplierOrToken } from '.'
 import type { GrammarTokenizerContext } from '~/shared/types'
 
 /**
@@ -13,7 +13,7 @@ export function getAtIdentifierOrToken(x: GrammarTokenizerContext) {
 
 	x.consumeCodeAt0(TOKEN.AT)
 
-	const name = getIdentifier(x).node
+	const name = getIdentifierName(x).node
 
 	if (x.codeAt0 === TOKEN.L_PARENTHESIS) {
 		x.consume(1)
